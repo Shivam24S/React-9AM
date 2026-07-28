430209765982a56f826a7f7ac17a7a140c0ebade
shivam24s <shivamshrivastav8055@gmail.com>
2026-07-28 10:05:40 +0530

    adding expense

--- 09_context_api_expense_tracker/src/components/AddExpense.jsx
+++ 09_context_api_expense_tracker/src/components/AddExpense.jsx
@@ -1,16 +1,129 @@
-import React, { useContext } from 'react'
-import { ExpenseContext } from '../context/ExpenseContext'
+import React, { useContext, useState } from "react";
 
 const AddExpense = () => {
+  const [input, setInput] = useState({
+    title: "",
+    description: "",
+    category: "",
+    amount: 0,
+    date: "",
+    type: "debit",
+  });
 
+  const [data, setData] = useState([]);
 
-    const { expenseList } = useContext(ExpenseContext)
+  const handleChange = (field, e) => {
+    setInput((prev) => {
+      return {
+        ...prev,
+        [field]: e.target.value,
+      };
+    });
+  };
 
-    console.log("expenseList", expenseList)
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    return (
-        <div>AddExpense</div>
-    )
-}
+    setData((prev) => [...prev, input]);
 
-export default AddExpense
+    setInput({
+      title: "",
+      description: "",
+      category: "",
+      amount: 0,
+        date: "",
+      type: "debit",
+    });
+  };
+
+  console.log("inputData", data);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">title</label>
+        <input
+          type="text"
+          placeholder="title"
+          name="title"
+          value={input.title}
+          onChange={(e) => handleChange("title", e)}
+        />
+        <br />
+        <br />
+        <label htmlFor="description">description</label>
+        <input
+          type="text"
+          placeholder="description"
+          name="description"
+          value={input.description}
+          onChange={(e) => handleChange("description", e)}
+        />
+        <br />
+        <br />
+        <label htmlFor="amount">amount</label>
+        <input
+          type="number"
+          placeholder="amount"
+          name="amount"
+          value={input.amount}
+          onChange={(e) => handleChange("amount", e)}
+        />
+
+        <br />
+        <br />
+
+        <label htmlFor="date">date</label>
+        <input
+          type="date"
+          name="date"
+          value={input.date}
+          onChange={(e) => handleChange("date", e)}
+        />
+
+        <br />
+        <br />
+
+        <label htmlFor="category">category</label>
+        <select
+          name="category"
+          value={input.category}
+          onChange={(e) => handleChange("category", e)}
+        >
+          <option value="Money Transfer">Money Transfer</option>
+          <option value="Cash Withdrawal">Cash Withdrawal</option>
+          <option value="General Expense">General Expense</option>
+          <option value="Food&Dining">Food&Dining</option>
+          <option value="HealthCare">HealthCare</option>
+          <option value="Shopping">Shopping</option>
+          <option value="Travel">Travel</option>
+        </select>
+        <br />
+        <br />
+
+        <label htmlFor="type">Expense type</label>
+        <select
+          name="type"
+          value={input.type}
+          onChange={(e) => handleChange("type", e)}
+        >
+          <option value="credit">credit</option>
+          <option value="debit">debit</option>
+        </select>
+
+        <br />
+        <br />
+        <button type="submit">add</button>
+      </form>
+    </>
+  );
+};
+
+export default AddExpense;
+
+// title,
+// description,
+// category,
+// amount,
+// type
+// date
